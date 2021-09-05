84a319933544dacd5a7f6f3ec9d7c6f2015e2a00
tiechui1994 <2904951429@qq.com>
2021-09-05 21:12:34 +0800

    update weixin test

--- weixin/weixin_test.go
+++ weixin/weixin_test.go
@@ -1,8 +1,11 @@
 package weixin
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+)
 
-var token = ""
+var token = "48_sU3K7Ezw5aL3Kyd4ChGcG_f-Mkm_bTij5jTv9u224hXkbMAXX7QBbvx4rLLX-lqd7JkbhqwlIQ5pbgKIlsledrmJdCmyFlXMuHkvAkWAYJcTmOb1L0OzHCSHsoz7izmjogWWoNTFkaDw43o3NSKdAEAGLD"
 
 func TestUploadNewsImage(t *testing.T) {
 	id, err := UploadNewsImage(token, "/home/quinn/Pictures/develop_runtime_cfun7.jpeg")
@@ -22,43 +25,15 @@ func TestMediaList(t *testing.T) {
 	t.Log("err", err)
 }
 
-// crAgK-cmXMbGEq9kZymbzc9iYfy8UIf4ng6epM602TM
+// crAgK-cmXMbGEq9kZymbzf_ZnZjo4XV7HHYef7yF_ZI
 func TestAddPersitNews(t *testing.T) {
+	data, _ := ioutil.ReadFile("/home/quinn/Desktop/www.html")
 	news := Article{
 		Title:            "测试当中",
 		Author:           "tiechui1994",
-		ThumbMediaID:     "crAgK-cmXMbGEq9kZymbzaHLCE_7aLLFwnWA99U598Y",
+		ThumbMediaID:     "crAgK-cmXMbGEq9kZymbzWP0k6ziJ5W_OayqgGzT2Go",
 		ContentSourceUrl: "https://www.baidu.com",
-		Content: `
-<!DOCTYPE html>
-<html>
-<head>
-<title>Welcome to nginx!</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-    p {
-		font-size: 30px;
-    }
-</style>
-</head>
-<body>
-<h1>Welcome to nginx!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
-
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
-
-<p><em>Thank you for using nginx.</em></p>
-</body>
-</html>
-		`,
+		Content: string(data),
 	}
 	id, err := AddPersitNews(token, news)
 	t.Log("id", id)
@@ -66,32 +41,20 @@ Commercial support is available at
 }
 
 func TestUpdatePersitNews(t *testing.T) {
+	data, _ := ioutil.ReadFile("/home/quinn/Desktop/yyy.html")
 	news := Article{
 		Title:            "测试当中",
 		Author:           "tiechui1994",
-		ThumbMediaID:     "crAgK-cmXMbGEq9kZymbzaHLCE_7aLLFwnWA99U598Y",
+		ThumbMediaID:     "crAgK-cmXMbGEq9kZymbzWP0k6ziJ5W_OayqgGzT2Go",
 		ContentSourceUrl: "https://www.baidu.com",
-		Content: `
-<div>
-<h1>Welcome to nginx!</h1>
-<p style="font-size: 30px;">If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
-
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
-
-<p><em>Thank you for using nginx.</em></p>
-</div>
-		`,
+		Content: string(data),
 	}
-	err := UpdatePersitNews(token, "crAgK-cmXMbGEq9kZymbzc9iYfy8UIf4ng6epM602TM", 0, news)
+	err := UpdatePersitNews(token, "crAgK-cmXMbGEq9kZymbzf_ZnZjo4XV7HHYef7yF_ZI", 0, news)
 	t.Log("err", err)
 }
 
 func TestGetPersitNews(t *testing.T) {
-	news, err := GetPersitNews(token, "crAgK-cmXMbGEq9kZymbzc9iYfy8UIf4ng6epM602TM")
+	news, err := GetPersitNews(token, "crAgK-cmXMbGEq9kZymbzfRNQraVlfgjkPuDLTrU2yY")
 	t.Log("news", news)
 	t.Log("err", err)
 }
