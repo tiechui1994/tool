1492a94493415ef3cf5c8d042af91ffc8d183945
tiechui1994 <2904951429@qq.com>
2021-09-06 09:19:21 +0800

    update idea

--- internal/cmd/idea.go
+++ internal/cmd/idea.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	var (
 		path string
+		code1, code2 bool
 	)
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
@@ -21,17 +22,45 @@ func main() {
 			Value:       "/root",
 			Destination: &path,
 		},
+		cli.BoolFlag{
+			Name:        "1",
+			Usage:       "code 1 website(idea.94goo.com)",
+			Destination: &code1,
+		},
+		cli.BoolFlag{
+			Name:        "2",
+			Usage:       "code 2 website(vrg123.com)",
+			Destination: &code2,
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		code, username, password := idea.GetCode2()
-		if !idea.ValidCode(code) {
-			log.Infoln("username:%v, password:%v, you can try", username, password)
-			code = idea.GetCode1()
+		if !code1 && !code2 {
+			code1  = true
 		}
-		if !idea.ValidCode(code) {
-			log.Errorln("no code")
-			os.Exit(1)
+
+		var code string
+		if code1 {
+			code = idea.GetCode1()
+			if !idea.ValidCode(code) {
+				var username, password string
+				code, username, password = idea.GetCode2()
+				log.Infoln("username:%v, password:%v, you can try", username, password)
+			}
+			if !idea.ValidCode(code) {
+				log.Errorln("no code")
+				os.Exit(1)
+			}
+		} else {
+			code, username, password := idea.GetCode2()
+			if !idea.ValidCode(code) {
+				log.Infoln("username:%v, password:%v, you can try", username, password)
+				code = idea.GetCode1()
+			}
+			if !idea.ValidCode(code) {
+				log.Errorln("no code")
+				os.Exit(1)
+			}
 		}
 
 		paths := idea.SearchFile(path)
